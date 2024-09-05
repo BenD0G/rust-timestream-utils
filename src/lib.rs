f8e1f09bc870bd8b3a0ef5e36ac183d296da4c15
Ben <ben.larson@hotmail.com>
2024-09-05 14:57:09 +0000

    Export TImestreamClients

--- src/lib.rs
+++ src/lib.rs
@@ -4,7 +4,7 @@ mod test_utils;
 
 pub use table::{
     load_timestream_clients, make_record_with_time, naive_date_from_timestamp,
-    utc_datetime_from_timestamp, TimestreamTable,
+    utc_datetime_from_timestamp, TimestreamClients, TimestreamTable,
 };
 
 #[cfg(feature = "test-utils")]
